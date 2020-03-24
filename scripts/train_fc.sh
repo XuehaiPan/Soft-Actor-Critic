e11c3e1584ed5be35a3ae05f2bb942d0c2bc84cb
XuehaiPan <XuehaiPan@pku.edu.cn>
2020-03-24 18:21:24 +0800

    chore: update hyper parameters

--- scripts/train_fc.sh
+++ scripts/train_fc.sh
@@ -13,7 +13,7 @@ python3 main.py --mode train --gpu 0 --env "$ENV" \
 	--net FC --activation LeakyReLU \
 	--hidden-dims 256 256 256 128 128 128 \
 	--max-episodes 4000 --max-episode-steps 500 \
-	--n-updates 256 --batch-size 64 \
+	--n-updates 256 --batch-size 256 \
 	--buffer-capacity 1000000 --lr 1E-4 --weight-decay 1E-5 --random-seed 0 \
 	--log-dir "logs/$ENV/FC" \
 	--checkpoint-dir "checkpoints/$ENV/FC"
--- scripts/train_rnn.sh
+++ scripts/train_rnn.sh
@@ -15,8 +15,8 @@ python3 main.py --mode train --gpu 0 --env "$ENV" \
 	--hidden-dims-lstm 128 \
 	--hidden-dims-after-lstm 128 128 \
 	--skip-connection \
-	--max-episodes 4000 --max-episode-steps 500 \
-	--n-updates 4 --batch-size 16 --max-step-size 32 \
+	--max-episodes 10000 --max-episode-steps 500 \
+	--n-updates 4 --batch-size 16 --max-step-size 20 \
 	--buffer-capacity 1000000 --lr 1E-4 --weight-decay 1E-5 --random-seed 0 \
 	--log-dir "logs/$ENV/RNN" \
 	--checkpoint-dir "checkpoints/$ENV/RNN"
