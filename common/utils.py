dc9d78ada6f4f0fc531de6c1bfe84b092562e238
XuehaiPan <XuehaiPan@pku.edu.cn>
2020-04-16 16:29:40 +0800

    fix: fix multiprocessing performance issues

--- common/utils.py
+++ common/utils.py
@@ -19,7 +19,8 @@ def clone_network(src_net, device=None):
 
 def sync_params(src_net, dst_net, soft_tau=1.0):
     if soft_tau == 1.0:
-        dst_net.load_state_dict(src_net.state_dict())
+        for src_param, dst_param in zip(src_net.parameters(), dst_net.parameters()):
+            dst_param.data.copy_(src_param.data)
     else:
         for src_param, dst_param in zip(src_net.parameters(), dst_net.parameters()):
             dst_param.data.copy_(dst_param.data * (1.0 - soft_tau) + src_param.data * soft_tau)
